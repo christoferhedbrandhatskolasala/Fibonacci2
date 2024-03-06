91c738a86b766983b6f13362957f3ed9d9ec00d8
Christofer Hedbrandh <christofer.hedbrandh@skola.sala.se>
2024-03-06 11:53:09 +0100

    Add project files.

---
+++ Fibonacci2/Form1.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace Fibonacci2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBox3 = new TextBox();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(306, 58);
+            textBox3.Multiline = true;
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(411, 225);
+            textBox3.TabIndex = 2;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(104, 214);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 29);
+            button1.TabIndex = 3;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button1);
+            Controls.Add(textBox3);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private TextBox textBox3;
+        private Button button1;
+    }
+}
---
+++ Fibonacci2/Form1.cs
@@ -0,0 +1,30 @@
+using Accessibility;
+
+namespace Fibonacci2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int prevPrevNumber = 0;
+            int prevNumber = 1;
+
+            textBox3.Text = "0 1 ";
+
+            for (int i = 0; i < 30; i++)
+            {
+                int newNumber = prevNumber + prevPrevNumber;
+                textBox3.Text = textBox3.Text + newNumber + " ";
+
+                prevPrevNumber = prevNumber;
+                prevNumber = newNumber;
+            }
+
+        }
+    }
+}
